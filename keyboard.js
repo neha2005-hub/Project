0887bfe5ce8090f8c3747e34eacfc1997da36ddf
Neha Kumari <neharnp2@gmail.com>
2026-06-10 15:38:53 +0530

    Add files via upload

--- keyboard.js
+++ keyboard.js
@@ -4,17 +4,21 @@ const keys = document.querySelectorAll(".key");
 keys.forEach(key => {
     key.addEventListener("click", () => {
         display.value += key.textContent;
+        display.focus();
     });
 });
 
 document.getElementById("space").addEventListener("click", () => {
     display.value += " ";
+    display.focus();
 });
 
 document.getElementById("clear").addEventListener("click", () => {
     display.value = "";
+    display.focus();
 });
 
 document.getElementById("backspace").addEventListener("click", () => {
     display.value = display.value.slice(0, -1);
+    display.focus();
 });
